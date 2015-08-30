e362bec0b8272204f5a74fe1e98a47859dc3ad17
07-UshiamaHideto <hi20.ushi@gmail.com>
2015-08-30 17:13:16 +0900

    最初のコミット

---
+++ 5/index.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+<form action="get_confirm.php" method="get">
+	名前: <input type="text" name="name" />
+	年齢: <input type="text" name="age" />
+	<input type="submit" />
+</form>
+<form action="post_confirm.php" method="post">
+	名前: <input type="text" name="name" />
+	年齢: <input type="text" name="age" />
+	<input type="submit" />
+</form>
+
+<form action="math_confirm.php" method="post">
+	<input type="text" name="m1" />+<input type="text" name="m2" />
+	<input type="submit" />
+</form>
+
+<ul>
+<li><a href="hensu.php">変数</a></li>
+<li><a href="hairetsu.php">配列</a></li>
+<li><a href="seigyo.php">制御構文</a></li>
+<li><a href="kansu.php">関数</a></li>
+</ul>
+
+<ul>
+<li><a href="write.php">ファイル書き込み</a></li>
+<li><a href="read.php">ファイル読み込む</a></li>
+</ul>
+
+</body>
+</html>
