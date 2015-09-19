942a6629ac3e57553d7b274c4f1a82d760f78966
07-UshiamaHideto <hi20.ushi@gmail.com>
2015-09-20 02:09:34 +0900

    一覧の管理ページを修正、ページ送りを追加

--- 6/admin/news_list.php
+++ 6/admin/news_list.php
@@ -35,7 +35,7 @@
 </div>
 
 <?php
-
+//表示順序
 if(isset($_POST["sl1"]) | isset($_POST["sl2"])){
 $j1 = $_POST["sl1"];
 $j2 = $_POST["sl2"];
@@ -51,16 +51,56 @@ if($j2=="降順"){
 }else{}
 
 }else{
-$db_set = "ORDER BY create_date DESC";
+	$db_set = "ORDER BY create_date DESC";
 }
+
+//表示数
+$set_n = 5;
+
+if(isset($_GET["page"])){
+	$page = intval($_GET["page"]);
+	if($page == 1){
+		$lo = " LIMIT " . $set_n;
+	}else{
+		$news_s = ($page - 1) * $set_n + 1;
+		$lo = " LIMIT " . $set_n . " OFFSET " . $news_s;
+	}
+}else{
+	$page = 1;
+	$news_s = ($page - 1) * $set_n + 1;
+	$lo = " LIMIT " . $set_n . " OFFSET " . $news_s;
+}
+
+
+//echo "limit:".$lo;
+
 //echo "db_set：".$db_set;
 $pdo = new PDO("mysql:host=localhost;dbname=cs_academy;charset=utf8", "root", "");
-//$sql = "SELECT * FROM news ORDER BY news_id DESC LIMIT 5";
 $sql = "SELECT * FROM news ".$db_set;
-
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$count=$stmt->rowCount();
+
+
+//$sql = "SELECT * FROM news ORDER BY news_id DESC LIMIT 5";
+$sql = "SELECT * FROM news ".$db_set.$lo;
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//行数（レコード数）をカウント
+
+echo "全ニュース記事数:".$count;
+
+if ($count > $set_n){
+	$n_page = (int) (intval($count) / intval($set_n));
+	$a_page = $count % $set_n;
+	if($a_page != 0){
+	$n_page++;
+	}
+}
+
+//echo "n_page".$n_page;
 
 //echo "<PRE style=\"text-align:left;\">";
 //var_dump($results);
@@ -74,13 +114,36 @@ foreach($results as $row) {
 	echo "<dl>";
 	echo "<a href=\"update.php?id=".$row["news_id"]."\">";
 	echo "<dd class=\"news-title\"><span>ID：</span>".$row["news_id"]."<span>　Title：</span>".mb_substr($row["news_title"],0,15)."</dd>";
-	echo "<dd><span>Detail：</span>".mb_substr($row["news_detail"],0,30)."</dd>";
+	echo "<dd><span>Detail：</span>".mb_substr($row["news_detail"],0,34)."</dd>";
 	echo "<div class=\"news_d\"><span>Date：</span>".$row["create_date"]."　<span>Author by</span> ".$row["author"]."</div>";
 	echo "</a>";
 	echo "</dl>";
 	echo "</article>";
 }
 $pdo = null;
+
+echo "<div class=\"page_l\">";
+if ($page != 1) {
+	echo "<a href=\"news_list.php?page=" . ($page - 1) . "\">前 <  </a>";
+}
+
+if ($n_page > 1) {
+	echo "<span class=\"page_n\">";
+	for($a=1;$a<=$n_page;$a++){
+		if($a==$page){
+			echo "<span class=\"page_e\">".$a."</span>";
+		}else{
+			echo "<span class=\"page_o\"><a href=\"news_list.php?page=".$a. "\">".$a."</a></span>";
+		}
+	}
+	echo "</span>";
+}
+
+if ($page != $n_page) {
+	echo "<a href=\"news_list.php?page=" . ($page + 1) . "\">  > 次</a>";
+}
+echo "</div>";
+
 ?>
 
 
