515788dce4ad089e6ad6a7c60a76e79eefccf998
07-UshiamaHideto <hi20.ushi@gmail.com>
2015-10-10 21:48:33 +0900

    ユーザ毎の投稿削除機能を追加

--- 10/js/main.js
+++ 10/js/main.js
@@ -1,12 +1,16 @@
 
 function loca(){
 
-//$(document).ready(function(){
+loc = "";
+lat = "";
+lon = "";
 
-$(function(){
-
+//$(function(){
+console.log("1 loc:"+loc);
 if (navigator.geolocation) {
-	loc = "";
+
+
+console.log("2 loc:"+loc);
 	// 現在の位置情報取得を実施
 	 navigator.geolocation.getCurrentPosition(
 	// 位置情報取得成功時
@@ -14,8 +18,9 @@ if (navigator.geolocation) {
 		lat = pos.coords.latitude;
 		lon = pos.coords.longitude;
 		loc = lat + " " +lon;
-	console.log("loc:"+loc);
-//		document.getElementById("location").innerHTML = location;
+	console.log("loc:"+loc+" lat:"+lat+" lon:"+lon);
+//		document.getElementById("location").innerHTML = loc;
+//		setMap(lat, lon);
 
 	},function (error) { // 位置情報取得失敗時
 		var mess = "";
@@ -35,16 +40,37 @@ if (navigator.geolocation) {
 		}
 		window.alert(mess);
 	});
-	console.log("out loc:"+loc);
+	console.log("3 loc:"+loc+" lat:"+lat+" lon:"+lon);
 } else {
 	window.alert("本ブラウザではGeolocationが使えません");
 }
 
+console.log("4 loc:"+loc);
 // });
 
+}
+/*
+//地図
+map = new google.maps.Map(
+document.getElementById("gmap"),{
+    zoom : 15,
+    center : new google.maps.LatLng(0,0),
+    mapTypeId : google.maps.MapTypeId.ROADMAP
 });
 
-}
+//マーカー
+marker = new google.maps.Marker({
+      position: new google.maps.LatLng(0,0),
+      map: map 
+    });
+
+//地図上に現在地を表示
+function setMap(lat, lon){
+    var pos = new google.maps.LatLng(lat, lon);
+    map.setCenter(pos);
+    marker.setPosition(pos);
+};
+*/
 
 //CHAT ログイン
 
@@ -62,6 +88,9 @@ g_user = localStorage.getItem(flag);
 	} else {
 //		console.log("user:"+g_user);
 		$("#u_post").css("display","none");
+//		$("#del_p").html("<div id=\"del_my\">"+g_user+"の投稿を削除</div> <div id=\"del_all\">全ての投稿を削除</div>");
+		$("#del_p").html("<div id=\"del_my\">"+g_user+"の投稿を削除</div>");
+		$("#content").before("<div class=\"u_text\">▶「"+g_user+"」名で投稿</div>");
 	}
 
 	$('#u_post_b').click(function () {
@@ -80,17 +109,38 @@ g_user = localStorage.getItem(flag);
 
 }
 
-//投稿数をカウント
+
+//投稿過程
+function post(ds) {
+loca();
+	console.log("on loc:"+loc);
+//	console.log("on g_user:"+g_user);
+	//5."message"データストアにメッセージをプッシュする
+var content = escapeHTML($("#content").val());
+if (content && content !== "") {
+	ds.push({
+	title: "タイトル",
+	user: g_user,
+	location: loc,
+	content: content,
+	date: new Date().getTime()
+	}, function (e) {});
+}
+$("#content").val("");
+t_co(ds);
+}
+
+//投稿数
 
 function t_co(ds){
 count = 0;
 //	console.log("ds:"+ds);
 ds.stream().sort("desc").next(function(err, datas) {
-	datas.forEach(function(data) {
-		count++
-	});
-	console.log("count1:"+count);
-	$("#num").html("投稿数:"+count);
+datas.forEach(function(data) {
+	count++
+});
+console.log("count1:"+count);
+$("#num").html("投稿数:"+count);
 });
 
 }
@@ -99,10 +149,14 @@ ds.stream().sort("desc").next(function(err, datas) {
 
 function t_wr(ds,me_s){
 //	console.log("me_s:"+me_s);
+	console.log("last_message:"+last_message);
+			var last_message ="dummy";
+	$("#mes").html("<div id=\""+last_message+"\"></div>");
 	ds.stream().sort("desc").size(me_s).next(function(err, datas) {
 //		console.log('desc');
 //		console.table(dates);
 		datas.forEach(function(data) {
+			var last_message ="dummy";
 			renderMessage(data);
 		});
 	});
@@ -133,3 +187,36 @@ function renderMessage(message) {
     function escapeHTML(val) {
 	return $('<div>').text(val).html();
     };
+
+//メッセージ削除
+	
+function del(ds){
+	ds.stream().sort("desc").next(function(err, datas) {
+//	console.log('desc');
+//	console.table(dates);
+		datas.forEach(function(data) {
+//		console.log("g_user:"+g_user); 
+		var m_id = data.id;
+		var c_na = data.value.user;
+		if (g_user==c_na){
+
+//			console.log("id:"+m_id+" value.user:"+c_na);
+			ds.remove(m_id, function(err, removed){
+//			$("#"+message.id).removel();
+			//サーバでの削除操作が完了
+//			console.log("del"+removed);
+			});
+		}
+		});
+		/*
+		{
+		id: 'test'
+		}
+		*/
+	}, function(err) {
+	//"Permission denied" or "limit exceeded"
+	});
+
+
+
+} 
