e022e055c0305b5d52ac0fc52d3b8859c48bdff8
07-UshiamaHideto <hi20.ushi@gmail.com>
2015-10-23 01:40:45 +0900

    chatアプリ改良版アップロード

---
+++ 11/js/main.js
@@ -0,0 +1,157 @@
+
+
+//CHAT ログイン
+
+function login_c(){
+
+var flag = "chat_user";
+g_user = localStorage.getItem(flag);
+
+//console.log("user:"+g_user);
+
+if(g_user == null){
+	$("#content").css("display","none");
+	$("#post").css("display","none");
+
+} else {
+//		console.log("user:"+g_user);
+	$("#u_post").css("display","none");
+//		$("#del_p").html("<div id=\"del_my\">"+g_user+"の投稿を削除</div> <div id=\"del_all\">全ての投稿を削除</div>");
+	$("#del_p").html("<div id=\"del_my\">"+g_user+"の投稿を削除</div>");
+	$("#content").before("<div class=\"u_text\">▶「"+g_user+"」名で投稿</div>");
+}
+
+$('#u_post_b').click(function () {
+var value = $("#user").val();
+if(value){
+	console.log("flag:"+flag+",user:"+value);
+	localStorage.setItem(flag, value);
+	$("#u_post").css("display","none");
+	$("#content").css("display","inline");
+	$("#post").css("display","inline");
+	g_user = value;
+}else{
+	alert("チャット名の入力がありません");
+}
+});
+
+}
+
+//投稿過程
+function post(ds) {
+loca();
+console.log("on loc:"+loc);
+//	console.log("on g_user:"+g_user);
+//	5."message"データストアにメッセージをプッシュする
+var content = escapeHTML($("#content").val());
+if (content && content !== "") {
+	ds.push({
+	title: "タイトル",
+	user: g_user,
+	location: loc,
+	content: content,
+	date: new Date().getTime()
+	}, function (e) {});
+}
+$("#content").val("");
+t_co(ds);
+}
+
+//投稿数
+
+function t_co(ds){
+count = 0;
+//	console.log("ds:"+ds);
+ds.stream().sort("desc").next(function(err, datas) {
+datas.forEach(function(data) {
+	count++
+});
+console.log("count1:"+count);
+$("#num").html("投稿数:"+count);
+});
+
+}
+
+//メッセージ読み込み
+
+function t_wr(ds,me_s){
+//	console.log("me_s:"+me_s);
+//	console.log("last_message:"+last_message);
+var last_message ="dummy";
+$("#mes").html("<div id=\""+last_message+"\"></div>");
+ds.stream().sort("desc").size(me_s).next(function(err, datas) {
+//	console.log('desc');
+//	console.table(dates);
+	datas.forEach(function(data) {
+		var last_message ="dummy";
+		renderMessage(data);
+	});
+});
+
+}
+
+//メッセージ書き込み
+
+function renderMessage(message) {
+var message_html = '<p class="post-text">' + escapeHTML(message.value.content) + '</p>';
+var date_html = '';
+var loc_html = '';
+//	console.log("user1:"+g_user);
+//	console.log("user2:"+message.value.user);
+if(message.value.date) {
+//	UNIX時間から日時表示に変換toLocaleString()
+	date_html = '<p class="post-date">DATE:'+escapeHTML( new Date(message.value.date).toLocaleString())+'</p>';
+}
+if(message.value.location) {
+//	位置情報
+	loc_m = message.value.location.split(" ");
+	console.log("f_loc:"+loc_m[0]+" "+loc_m[1]);
+	setMap2(loc_m[0], loc_m[1]);
+	loc_html = '<p class="post-loc">Locatin:'+escapeHTML(message.value.location)+'</p>';
+}
+if(message.value.user == g_user){
+	css_html = " my_text";
+}else{
+	css_html = " o_text";
+}
+$("#"+last_message).before('<div id="'+message.id+'" class="post'+ css_html +'"><span>'+ escapeHTML(message.value.user) +'</span>'+message_html + loc_html + date_html +'</div>');
+last_message = message.id;
+}
+
+//インジェクション対策
+function escapeHTML(val) {
+	return $('<div>').text(val).html();
+};
+
+//メッセージ削除
+	
+function del(ds){
+ds.stream().sort("desc").next(function(err, datas) {
+//	console.log('desc');
+//	console.table(dates);
+	datas.forEach(function(data) {
+//	console.log("g_user:"+g_user); 
+var m_id = data.id;
+var c_na = data.value.user;
+if (g_user==c_na){
+
+//	console.log("id:"+m_id+" value.user:"+c_na);
+	ds.remove(m_id, function(err, removed){
+//	$("#"+message.id).removel();
+//	サーバでの削除操作が完了
+//	console.log("del"+removed);
+	});
+}
+});
+/*
+{
+	id: 'test'
+}
+*/
+}, function(err) {
+//	"Permission denied" or "limit exceeded"
+});
+
+
+
+} 
---
+++ 11/js/maps.js
@@ -0,0 +1,152 @@
+function loca(){
+
+
+//$(function(){
+//console.log("1 loc:"+loc);
+if (navigator.geolocation) {
+
+
+//console.log("2 loc:"+loc);
+//	現在の位置情報取得を実施
+navigator.geolocation.getCurrentPosition(
+//	位置情報取得成功時
+function (pos) { 
+lat = pos.coords.latitude;
+lon = pos.coords.longitude;
+loc = lat + " " +lon;
+console.log("loc:"+loc+" lat:"+lat+" lon:"+lon);
+//	document.getElementById("location").innerHTML = loc;
+	setMap(lat, lon);
+
+},function (error) { // 位置情報取得失敗時
+var mess = "";
+switch (error.code) {
+//	位置情報取得できない場合
+case error.POSITION_UNAVAILABLE:
+	mess = "位置情報の取得ができませんでした。";
+	break;
+//	Geolocation使用許可されない場合
+case error.PERMISSION_DENIED:
+	mess = "位置情報取得の使用許可がされませんでした。";
+	break;
+//	タイムアウトした場合 
+case error.PERMISSION_DENIED_TIMEOUT:
+	mess = "位置情報取得中にタイムアウトしました。";
+	break;
+}
+	window.alert(mess);
+});
+//	console.log("3 loc:"+loc+" lat:"+lat+" lon:"+lon);
+} else {
+	window.alert("本ブラウザではGeolocationが使えません");
+}
+
+console.log("4 loc:"+loc);
+// });
+
+//地図
+map = new google.maps.Map(
+	document.getElementById("maps"),{
+	zoom : 15,
+	center : new google.maps.LatLng(0,0),
+	mapTypeId : google.maps.MapTypeId.ROADMAP
+	}
+);
+
+//マーカー
+marker = new google.maps.Marker({
+	position: new google.maps.LatLng(0,0),
+	map: map,
+	title: "自分"
+});
+
+marker2 = new google.maps.Marker({
+	position: new google.maps.LatLng(lat2,lat2),
+	map: map ,
+	title: "ほかの人"
+});
+
+}
+
+//地図上に現在地を表示
+function setMap(lat, lon){
+	var pos = new google.maps.LatLng(lat, lon);
+	map.setCenter(pos);
+	marker.setPosition(pos);
+};
+
+function setMap2(lat2, lon2){
+	var pos = new google.maps.LatLng(lat2, lon2);
+//	map.setCenter(pos);
+	marker2.setPosition(pos);
+};
+
+// }
+
+//位置移動
+function toPanmap(lat2, lon2) {
+  map.panTo(new google.maps.LatLng(lat2, lon2));
+}
+
+//住所から検索
+function getLatLng() {
+var geocoder = new google.maps.Geocoder();
+geocoder.geocode({
+		address: place,
+		region: 'jp'
+	}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			var bounds = new google.maps.LatLngBounds();
+			for (var r in results) {
+			if (results[r].geometry) {
+				var latlng = results[r].geometry.location;
+				bounds.extend(latlng);
+				var address = results[r].formatted_address.replace(/^日本, /, '');
+				$("#searh_r").prepend("<div class=\"search_w\"><button id=\"result_"+r+"\" value=\""+latlng+"\"><div class=\"result_t\">"+address+"<br>"+latlng+"</div></button></div>");
+
+				f = r;
+				adr = address;
+				search_s();
+
+/*				f = r;
+				$.each(f,function(){
+					var ml = "#result_" + this;
+					    $(ml).on("click",function(){
+					     var s_loc = $(ml).val();
+					     localStorage.setItem(address, s_loc);
+					    });
+				});*/
+				
+				new google.maps.InfoWindow({
+				    content: address //+ "<br>(Lat, Lng) = " + latlng.toString()
+				}).open(map, new google.maps.Marker({
+				    position: latlng,
+				    map: map
+				}));
+			    }
+			}
+			map.fitBounds(bounds);
+		} else if (status == google.maps.GeocoderStatus.ERROR) {
+			alert("サーバとの通信時にエラー！");
+		} else if (status == google.maps.GeocoderStatus.INVALID_REQUEST) {
+			alert("リクエスト問題,geocode()に渡すGeocoderRequestを確認");
+		} else if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+			alert("短時間にクエリを送りすぎ");
+		} else if (status == google.maps.GeocoderStatus.REQUEST_DENIED) {
+			alert("このページのジオコーダの利用が不許可");
+		} else if (status == google.maps.GeocoderStatus.UNKNOWN_ERROR) {
+			alert("サーバ側でなんらかのトラブルが発生");
+		} else if (status == google.maps.GeocoderStatus.ZERO_RESULTS) {
+			alert("見つかりません");
+		} else {
+			alert("？");
+		}
+	});
+};
+
+$("#search_g").on("click",function(){
+	place =$("#address").val();
+	console.log("place:"+place);
+	getLatLng(place);
+});
+
